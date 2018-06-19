09cd2b860887bd3397ca514c915012c4e03973c5
Nam-Quang Tran <tran.nam.quang@web.de>
2018-06-19 10:47:12 +0700

    Website deployment script now sets correct file permissions before deploying.

--- deploy-website.sh
+++ deploy-website.sh
@@ -9,4 +9,12 @@ die () {
 
 ./build-website.py
 
+cd dist/website
+find . -type f -print0 | xargs -0 chmod a+r
+find . -type f -print0 | xargs -0 chmod u+w
+find . -type f -print0 | xargs -0 chmod go-w
+find . -type f -print0 | xargs -0 chmod a-x
+chmod a+x index.php
+cd ../..
+
 rsync -avP -e ssh dist/website/ $1@web.sourceforge.net:/home/project-web/docfetcher/htdocs
