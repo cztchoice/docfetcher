4461a7c27e59a6cf792bd4ce2dd8c29f5823b027
Nam-Quang Tran <tran.nam.quang@web.de>
2019-01-07 10:18:46 +0100

    More detailed comments in the DocFetcher.bat file.

--- dist/launchers/launcher.bat
+++ dist/launchers/launcher.bat
@@ -1,16 +1,38 @@
 @echo off
 
 rem  Note: This file is an alternative launcher for DocFetcher. How to use it:
-rem  1) Modify this file as needed.
-rem  2) Move this file one level up into the DocFetcher folder.
+rem  1) Move this file one level up into the DocFetcher folder.
+rem  2) Modify this file as needed.
 rem  3) Double-click on this file to launch DocFetcher.
 rem  
-rem  Common modifications:
-rem  1) Replace the "java" keyword in the last line with the full path to the
-rem  Java executable.
-rem  2) Give DocFetcher more memory with the setting -Xmx..m in the last line.
-rem  For example, with -Xmx512m, DocFetcher will use up to 512 MB of memory.
-rem  Using more memory than about 1 GB requires a 64-bit Java runtime.
+rem  Common modifications of this file:
+rem  
+rem  1) You can give DocFetcher more memory with the setting -Xmx... in the last
+rem     line. For example, -Xmx512m will give DocFetcher up to 512 MB of memory,
+rem     whereas -Xmx8g will give DocFetcher up to 8 GB of memory. The latter
+rem     only works if your machine has at least 8 GB of RAM. Also, note that
+rem     using more memory than about 1 GB requires a 64-bit Java runtime.
+rem  
+rem  2) You can replace the "java" keyword in the last line with an absolute or
+rem     relative path to the Java executable. If the path contains spaces, put
+rem     the entire path in quotes, e.g.:
+rem     "C:\Program Files\Java\jre1.8.0_181\bin\java.exe"
+rem     Here's an example of a relative path to a Java runtime that resides in
+rem     the same parent folder as the DocFetcher folder:
+rem     ..\Java\bin\java.exe
+rem     Another relative path example where the Java folder resides directly
+rem     inside the DocFetcher folder:
+rem     .\Java\bin\java.exe
+rem     Setting an alternative Java path is useful for using a different
+rem     installed Java runtime, or for using a portable Java runtime such as the
+rem     jPortable runtime provided by the PortableApps project:
+rem     https://portableapps.com/apps/utilities/java_portable
+rem  
+rem  3) To hide the black command prompt window after launching, replace the
+rem     "java" keyword in the last line with: "start /b javaw". Notice the use
+rem     of "javaw" instead of "java". This also works with full paths to the
+rem     Java runtime, but in that case requires some extra batch file weirdness:
+rem     start /b "" "C:\Program Files\Java\jre1.8.0_181\bin\javaw.exe"
 
 cd %~dp0
 
