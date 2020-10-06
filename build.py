a41e9323d304df67f1507be644de6909ffb0f83d
Nam-Quang Tran <tran.nam.quang@web.de>
2020-10-06 20:55:40 +0200

    Updated build script.

--- build.py
+++ build.py
@@ -1,87 +1,104 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-'''
+"""
 This is not the actual build script. The real "build script" is a small Java
-program in the package 'net.sourceforge.docfetcher.build'. This script merely
+program in the package "net.sourceforge.docfetcher.build". This script merely
 compiles and launches the Java builder.
-'''
+"""
 
-import os, shutil, platform
-from os.path import exists, join, isfile, isdir
+import os, os.path as osp, platform, shutil, sys
 
-is_windows = 'windows' in platform.system().lower()
-classpath_sep = ';' if is_windows else ':'
+is_windows = "windows" in platform.system().lower()
+classpath_sep = ";" if is_windows else ":"
 
-print('Cleaning build directory...')
-if not exists('build'):
-	os.makedirs('build')
-for filename in os.listdir('build'):
-	path = join('build', filename)
-	if isfile(path):
+os.chdir(osp.dirname(sys.argv[0]))
+if osp.isfile("build"):
+	print("Can't create build directory, a file with that name already exists.")
+	exit(1)
+
+print("Cleaning build directory...")
+os.makedirs("build", exist_ok=True)
+for filename in os.listdir("build"):
+	path = osp.join("build", filename)
+	if osp.isfile(path):
 		os.remove(path)
-	elif isdir(path):
+	elif osp.isdir(path):
 		shutil.rmtree(path)
 
-print('Copying sources to build directory...')
+print("Copying sources to build directory...")
 shutil.copytree(
-	'src',
-	'build/tmp/src-builder',
-	ignore = shutil.ignore_patterns('.svn', '.cvs', '.git')
+	"src",
+	"build/tmp/src-builder",
+	ignore = shutil.ignore_patterns(".svn", ".cvs", ".git")
 )
 
-def execute(cmd_parts):
-	os.system(' '.join(cmd_parts))
+# *args -> [string]
+def flatten_args(*args):
+	args2 = []
+	for arg in args:
+		if isinstance(arg, (list, tuple)):
+			args2 += flatten_args(*arg)
+		else:
+			args2.append(str(arg))
+	return args2
+
+# ([string] | *string) -> int
+def call(*args):
+	import subprocess as sub
+	p = sub.Popen(flatten_args(*args))
+	p.communicate()
+	return p.wait()
 
 # Recursively collect library jars
 jars = []
-for root, dirs, files in os.walk('lib'):
+for root, dirs, files in os.walk("lib"):
 	for filename in files:
-		if not filename.endswith('.jar'): continue
-		jars.append(join(root, filename))
-
-package = 'net.sourceforge.docfetcher'
-package_path = package.replace('.', '/')
-
-print('Compiling sources...')
-compile_paths = [
-	join('build/tmp/src-builder',\
-	package_path, 'build/BuildMain.java')
-]
-for root, dirs, files in os.walk('build/tmp/src-builder'):
-	for filename in files:
-		if not filename.endswith('.java'):
+		if not filename.endswith(".jar"):
 			continue
-		if filename.startswith('Test') or filename.endswith('Test.java'):
-			path = join(root, filename)
+		jars.append(osp.join(root, filename))
+
+package = "net.sourceforge.docfetcher"
+package_path = package.replace(".", "/")
+
+print("Compiling sources...")
+compile_paths = [
+	osp.join("build/tmp/src-builder", package_path, "build/BuildMain.java")
+]
+for root, dirs, files in os.walk("build/tmp/src-builder"):
+	for filename in files:
+		if not filename.endswith(".java"):
+			continue
+		if filename.startswith("Test") or filename.endswith("Test.java"):
+			path = osp.join(root, filename)
 			compile_paths.append(path)
-execute([
-	'javac',
-	'-source 1.7',
-	'-target 1.7',
-	'-sourcepath build/tmp/src-builder',
-	'-classpath \"%s\"' % classpath_sep.join(jars),
-	'-nowarn',
-	'-encoding', 'utf8', # Needed for some Tika source files
-	' '.join(compile_paths)
-])
+call(
+	"javac",
+	"-source", "1.7",
+	"-target", "1.7",
+	"-sourcepath", "build/tmp/src-builder",
+	"-classpath", classpath_sep.join(jars),
+	"-nowarn",
+	"-encoding", "utf8", # Needed for some Tika source files
+	compile_paths
+)
 
-jar_path = 'build/tmp/docfetcher-builder.jar'
-main_class = package + '.build.BuildMain'
+jar_path = "build/tmp/docfetcher-builder.jar"
+main_class = package + ".build.BuildMain"
 
-print('Creating builder jar...')
-execute([
-	'jar cfe',
+print("Creating builder jar...")
+call(
+	"jar", "cfe",
 	jar_path,
 	main_class,
-	'-C build/tmp/src-builder .'
-])
+	"-C", "build/tmp/src-builder", "."
+)
 
-print('Launching builder...')
-print('-' * 40)
+print("Launching builder...")
+print("-" * 40)
 jars.append(jar_path)
-execute([
-	'java',
-	'-enableassertions', # required by test classes
-	'-classpath \"%s\"' % classpath_sep.join(jars),
+call(
+	"java",
+	"-enableassertions", # required by test classes
+	"-classpath", classpath_sep.join(jars),
 	main_class
-])
+)
